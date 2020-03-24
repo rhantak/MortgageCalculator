b1708a5179d4b5202c8753d0333be5c06b75db8d
Ryan Hantak <rhantak@gmail.com>
2020-03-24 15:52:42 -0600

    Refactor use of one scanner

--- src/com/hantak/Main.java
+++ src/com/hantak/Main.java
@@ -5,16 +5,17 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
         final int MONTHS_IN_YEAR = 12;
         final int NUM_TO_PERCENT = 100;
 
         System.out.println("Welcome to Mortgage Calculator!");
 
-        int principal = inputPrincipal();
+        int principal = inputPrincipal(scanner);
 
-        double monthlyInterest = inputAnnualInterest() / MONTHS_IN_YEAR / NUM_TO_PERCENT;
+        double monthlyInterest = inputAnnualInterest(scanner) / MONTHS_IN_YEAR / NUM_TO_PERCENT;
 
-        int mortgageMonths = inputMortgagePeriod() * MONTHS_IN_YEAR;
+        int mortgageMonths = inputMortgagePeriod(scanner) * MONTHS_IN_YEAR;
 
         double numerator = monthlyInterest * Math.pow((1+monthlyInterest), mortgageMonths);
         double denominator = Math.pow((1+monthlyInterest), mortgageMonths) - 1;
@@ -26,8 +27,7 @@ public class Main {
         System.out.println("Monthly Payment: " + monthlyPayment);
     }
 
-    public static int inputPrincipal() {
-        Scanner scanner = new Scanner(System.in);
+    public static int inputPrincipal(Scanner scanner) {
         int principal = 0;
         while(principal <= 1_000 || principal >= 1_000_000) {
             System.out.println("Mortgage principal must be between $1,000 and $1,000,000");
@@ -37,8 +37,7 @@ public class Main {
         return principal;
     }
 
-    public static double inputAnnualInterest() {
-        Scanner scanner = new Scanner(System.in);
+    public static double inputAnnualInterest(Scanner scanner) {
         double annualInterest = 0;
         while (annualInterest <=0 || annualInterest > 30) {
             System.out.println("Annual interest rate must be greater than 0% and less than or equal to 30%");
@@ -48,8 +47,7 @@ public class Main {
         return annualInterest;
     }
 
-    public static int inputMortgagePeriod() {
-        Scanner scanner = new Scanner(System.in);
+    public static int inputMortgagePeriod(Scanner scanner) {
         int mortgageMonths = 0;
         while (mortgageMonths < 12 || mortgageMonths > 360) {
             System.out.println("Mortgage period must be between 1 year to 30 years");
